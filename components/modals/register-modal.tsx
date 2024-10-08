72fc328da1d85c4b5730fd808568ef9591fcc771
Islomjonsdev <islomjon@immigrateable.com>
2024-10-08 11:07:42 +0500

    modal done

--- components/modals/register-modal.tsx
+++ components/modals/register-modal.tsx
@@ -3,18 +3,24 @@
 import useRegisterModal from '../hooks/useRegisterModal'
 import Modal from '../ui/modal'
 import twitterIcon from "../../public/images/x.svg"
+import { useState } from 'react'
 
 const RegisterModal = () => {
+  const [ step, setStep ] = useState(1)
   const registerModal = useRegisterModal()
-  const bodyContent = (
-    <div className='flex flex-col space-y-4 '>
-      
-    </div>
-  )
+  const bodyContent = step === 1 ? <RegisterStep1 /> : <RegisterStep2 />
   const footer = <div>Footer content</div>
   return (
-    <Modal body={bodyContent} footer={footer} isOpen={registerModal.isOpen} onClose={registerModal.onClose} />
+    <Modal body={bodyContent} footer={footer} isOpen={registerModal.isOpen} onClose={registerModal.onClose} step={step} totalStep={2}/>
   )
 }
 
 export default RegisterModal
+
+const RegisterStep1 = () => {
+  return <div>Register step1</div>
+}
+
+const RegisterStep2 = () => {
+  return <div>Register step1</div>
+}
--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -7,17 +7,23 @@ interface ModalProps {
     onClose?: () => void;
     body?: ReactElement;
     footer?: ReactElement;
+    step: number,
+    totalStep?: number,
 }
 
-const Modal = ({ isOpen, onClose, body, footer, }: ModalProps) => {
+const Modal = ({ isOpen, onClose, body, footer, step, totalStep }: ModalProps) => {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent className="bg-black p-1">
                 <div className="flex items-center gap-6">
                     <button className='p-1 border-0 text-white hover:opacity-70 transition w-fit' onClick={onClose}>
-                        <X size={28}/>
+                        <X size={28} />
                     </button>
+                    {step && totalStep && (
+                        <div className="text-xl font-bold">Step {step} of {totalStep}</div>
+                    )}
                 </div>
+                <div className="mt-4">{body}</div>
                 {footer && <div className="mt-4">{footer}</div>}
             </DialogContent>
         </Dialog>
